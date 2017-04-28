9069b7d5096a9c1e080c9709e289bd9cf7bbe41f
olia-la <olga.dmitrochenkova@gmail.com>
2017-04-28 18:28:53 +0200

    Introduce cities array and plus button

--- src/Cities/Cities.js
+++ src/Cities/Cities.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 import City from './City';
 
 class Cities extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cities: [
+        {id: 1, name: 'Berlin', description: 'placeholder'},
+        {id: 2, name: 'Paris', description: 'test'}
+      ]
+    }
+  }
   render() {
     return (
       <div>
-      <City name='Berlin' description='Berlin is the capital of Germany. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treat' />
-      <City name='Paris' description='Paris has some of the most beautiful museums in the world.' />
-      <City name='Rome' description='Rome is the capital of Italy and it is full of traffic.' />
-      <City name='Amsterdam' description='Amsterdam has canals and marijuana 😂.' />
+        <Button basic icon='add circle' onClick={this.handleOnClick} />
+        {this.state.cities.map((city) => <City name={city.name} description={city.description} key={city.id}/>)}
+        
       </div>
     )
   }
