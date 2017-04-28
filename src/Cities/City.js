c7f624536589644f903be1a67d115a9bf49343d3
olia-la <olga.dmitrochenkova@gmail.com>
2017-04-28 13:14:16 +0200

    Improve change header

--- src/Cities/City.js
+++ src/Cities/City.js
@@ -3,12 +3,17 @@ import { Card, Icon, Form, Button, TextArea } from 'semantic-ui-react';
 
 class City extends Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
-      editOn: false
+      editOn: false,
+      previousName: props.name,
+      name: props.name
     };
     this.handleOnClick = this.handleOnClick.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
   }
 
   handleOnClick(e) {
@@ -16,13 +21,33 @@ class City extends Component {
     this.setState({editOn: !this.state.editOn});
   }
 
+  handleNameChange(e) {
+    this.setState({name: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      editOn: !this.state.editOn,
+      previousName: this.state.name
+    });
+  }
+
+  handleCancel(e) {
+    e.preventDefault();
+    this.setState({
+      name: this.state.previousName,
+      editOn: !this.state.editOn
+    });
+  }
+
   render() {
     if (this.state.editOn) {
       return (
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <Card>
-              <Card.Header><Form.Input defaultValue={this.props.name} /></Card.Header>
-               <Card.Description><TextArea defaultValue={this.props.description} /></Card.Description>
+              <Card.Header><Form.Input value={this.state.name} onChange={this.handleNameChange} /></Card.Header>
+              //  <Card.Description><TextArea value={this.state.value} onChange={this.handleChange} /></Card.Description>
                <Card.Content extra>
                  <Icon name='marker' />
                  12 places to visit
@@ -30,7 +55,7 @@ class City extends Component {
             <Card.Content>
               <div className='ui two buttons'>
                 <Button basic color='grey'>Save</Button>
-                <Button basic color='grey' onClick={this.handleOnClick}>Cancel</Button>
+                <Button basic color='grey' onClick={this.handleCancel}>Cancel</Button>
               </div>
            </Card.Content>
           </Card>
@@ -40,7 +65,7 @@ class City extends Component {
 
     return (
       <Card>
-       <Card.Content header={this.props.name} />
+       <Card.Content header={this.state.name} />
         <Card.Content description={this.props.description} />
        <Card.Content extra>
          <Icon name='marker' />
