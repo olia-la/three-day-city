ac3cd6a1e3f852c29d6e5598d2a640e54713145d
olia-la <olga.dmitrochenkova@gmail.com>
2017-04-28 10:52:16 +0200

    Add better layout

--- src/Cities/Cities.js
+++ src/Cities/Cities.js
@@ -5,7 +5,7 @@ class Cities extends Component {
   render() {
     return (
       <div>
-      <City name='Berlin' description='Berlin is the capital of Germany.' />
+      <City name='Berlin' description='Berlin is the capital of Germany. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treat' />
       <City name='Paris' description='Paris has some of the most beautiful museums in the world.' />
       <City name='Rome' description='Rome is the capital of Italy and it is full of traffic.' />
       <City name='Amsterdam' description='Amsterdam has canals and marijuana 😂.' />
--- src/Cities/City.js
+++ src/Cities/City.js
@@ -21,15 +21,13 @@ class City extends Component {
       return (
         <Form>
           <Card>
-            <Card.Content>
-              <Card.Header><Form.Input label='City' placeholder='Enter city name' /></Card.Header>
-               <Card.Description><TextArea placeholder='Add description' /></Card.Description>
-               <Card.Content>
+              <Card.Header><Form.Input defaultValue={this.props.name} /></Card.Header>
+               <Card.Description><TextArea defaultValue={this.props.description} /></Card.Description>
+               <Card.Content extra>
                  <Icon name='marker' />
                  12 places to visit
                </Card.Content>
-            </Card.Content>
-            <Card.Content extra>
+            <Card.Content>
               <div className='ui two buttons'>
                 <Button basic color='grey'>Save</Button>
                 <Button basic color='grey' onClick={this.handleOnClick}>Cancel</Button>
@@ -42,19 +40,17 @@ class City extends Component {
 
     return (
       <Card>
+       <Card.Content header={this.props.name} />
+        <Card.Content description={this.props.description} />
+       <Card.Content extra>
+         <Icon name='marker' />
+         12 places to visit
+       </Card.Content>
         <Card.Content>
-          <Card.Header>{this.props.name}</Card.Header>
-           <Card.Description>{this.props.description}</Card.Description>
-           <Card.Content>
-             <Icon name='marker' />
-             12 places to visit
-           </Card.Content>
-        </Card.Content>
-        <Card.Content extra>
           <div className='ui two buttons'>
             <Button basic color='grey' onClick={this.handleOnClick}>Edit</Button>
           </div>
-       </Card.Content>
+        </Card.Content>
       </Card>
     )
   }
